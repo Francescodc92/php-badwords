7fc8370059d14ae74c96d5d59622499c3f309a92
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-08-21 19:12:48 +0200

    add text and langht to file php

--- badwords.php
+++ badwords.php
@@ -1,3 +1,9 @@
+<?php 
+  $paragraf = $_POST['paragraf'];
+  $badword = $_POST['badword'];
+?>
+
+
 <!DOCTYPE html>
 <html lang="it">
 <head>
@@ -8,8 +14,22 @@
   <title>PHP Badwords</title>
 </head>
 <body>
-  <h1>
-    hello
-  </h1>
+  <div class="result-wrapper">
+    <h2>Testo senza censura</h2>
+    <p>
+      <?= $paragraf ?>
+    </p>
+    <?php 
+      echo "<p>
+      lungezza testo:
+        <strong>"
+          .strlen($paragraf)."
+        </strong>
+      </p>"
+    ?>
+    <h2>
+      Testo censurato
+    </h2>
+  </div>
 </body>
 </html>
